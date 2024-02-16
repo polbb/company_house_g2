4e6652018ca478651c888238f340c11b91608edc
polbb <paulbessone@gmail.com>
2024-02-16 09:05:00 +0000

    Table fix

--- src/scripts/streamlit_xhtml.py
+++ src/scripts/streamlit_xhtml.py
@@ -161,10 +161,11 @@ def streamlit_xhtml(company_number):
     with tab4:
         # Benford's Law application with frequencies in percentage
         first_digit_frequencies = benford(company_number)
-        first_digit_df = pd.DataFrame(first_digit_frequencies.items(), columns=['First Digit', 'Frequency (%)'])
+        first_digit_df = pd.DataFrame(list(first_digit_frequencies.items()), columns=['First Digit', 'Frequency (%)'])
         
-        # Display the frequencies in a table with named columns
-        st.table(first_digit_df.reset_index(drop=True))  # Remove the index column
+        # Display the frequencies in a table with named columns without removing the index column
+        st.table(first_digit_df)
+        
     with tab1:
             
         if 'dataframe' in locals() or 'dataframe' in globals():
