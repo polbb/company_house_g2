123da28f1cb5a19fb3d6dfa423dce24769ff8bcd
polbb <paulbessone@gmail.com>
2024-02-16 09:02:14 +0000

    Table fix

--- src/scripts/streamlit_xhtml.py
+++ src/scripts/streamlit_xhtml.py
@@ -161,7 +161,7 @@ def streamlit_xhtml(company_number):
     with tab4:
         # Benford's Law application with frequencies in percentage
         first_digit_frequencies = benford(company_number)
-        first_digit_df = pd.DataFrame(first_digit_frequencies.items(), columns=['First Digit', 'Frequency (%)'], index=None)
+        first_digit_df = pd.DataFrame(first_digit_frequencies.items(), columns=['First Digit', 'Frequency (%)'])
         
         # Display the frequencies in a table with named columns
         st.table(first_digit_df)
